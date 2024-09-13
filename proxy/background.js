565aeee19a0b30163308401798ac6b4201167931
Trinh253 <trinhtt001@gmail.com>
2024-09-13 14:15:53 +0700

    add

---
+++ proxy/background.js
@@ -0,0 +1,27 @@
+
+    var config = {
+    mode: "fixed_servers",
+    rules: {
+      singleProxy: {
+        scheme: "http",
+        host: "103.139.44.201",
+        port: parseInt("5677")
+      },
+    bypassList: [""]
+    }
+  };
+chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+function callbackFn(details) {
+    return {
+        authCredentials: {
+            username: "MVN06574",
+            password: "sFiP8q5r"
+        }
+    };
+}
+chrome.webRequest.onAuthRequired.addListener(
+        callbackFn,
+        {urls: ["<all_urls>"]},
+        ['blocking']
+);
+    
